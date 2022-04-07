f82c6f2df0035fdd599937f2f7e2136ed8a740ba
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-04-07 16:47:22 -0700

    added generatePassword function plus length criteria

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,10 @@
 // Assignment code here
 
-// Arrays containing every possible character
- var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
- var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
- var characters = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", ",", ".", "/", ":", ";", "<", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
-
- var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
  // Main password generating function
+  
 
-
-// Password length criteria 
+// // Password length criteria 
 
 
 // Character type confirmation
@@ -24,6 +16,33 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+ var symbols = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", ",", ".", "/", ":", ";", "<", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+ var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+var lengthPrompt = "";
+
+function generatePassword() {
+  console.log("Button worked");
+  var lengthPrompt = window.prompt("Between 8 and 128 characters, how long would you like your password to be?");
+  if (lengthPrompt == "" || lengthPrompt == null) {
+    alert("Click to reset this.");
+    return;
+  } while (lengthPrompt <= 8 || lengthPrompt >= 128) {
+    window.alert("Quantity entered must be between 8 and 128");
+    var lengthPrompt = window.prompt("Between 8 and 128 characters, how long would you like your password to be?");
+  }
+  
+  var uppercaseCriteria;
+  var lowercaseCriteria;
+  var numbersCriteria;
+  var symmbolsCriteria;
+};
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -31,10 +50,11 @@ function writePassword() {
 
   passwordText.value = password;
 
+  
+
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-console.log(upperCase.length)
