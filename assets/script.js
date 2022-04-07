423878a572bf7890a2ebf620d00143b6d629c65e
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-04-06 19:39:29 -0700

    added Checklist to HTML plus styled Checklist and added array of all characters to JS

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,26 @@
 // Assignment code here
 
+// Arrays containing every possible character
+ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+ var characters = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", ",", ".", "/", ":", ";", "<", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+ var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+ // Main password generating function
+
+
+// Password length criteria 
+
+
+// Character type confirmation
+
+
+// When all prompts are answered the password is then generated
+
+ 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +35,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+console.log(upperCase.length)
